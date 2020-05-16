d227455d45e8f162af79d61ffe7a35b8b6cd72bc
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-05-16 17:49:02 -0500

    Basic scene

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  trailingComma: "none",
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+};
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <paint-canvas class="paint-canvas" />
+  </div>
+</template>
+
+<script>
+import PaintCanvas from './components/PaintCanvas';
+
+export default {
+  name: 'App',
+  components: {
+    PaintCanvas
+  }
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  height: 100%;
+
+  .paint-canvas {
+    flex: 2;
+    height: 100%;
+  }
+}
+</style>
