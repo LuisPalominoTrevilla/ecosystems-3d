fc11ac37ba4f1f929eaea15e96d65f023f8e8db5
Luis Palomino Trevilla <luispalominot3@gmail.com>
2020-05-20 23:55:18 -0500

    Removed test ecosystem

--- src/ecosystems/index.js
+++ src/ecosystems/index.js
@@ -1,7 +1,5 @@
-import TestEcosystem from './test-ecosystem';
 import CoralReefEcosystem from './coral-reef-ecosystem';
 
 export default {
-  testEcosystem: new TestEcosystem(),
   coralReefEcosystem: new CoralReefEcosystem()
 };
