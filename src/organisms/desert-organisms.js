8fb481b25def7958e350d550d9305c3121b98a8a
Juan Carlos Quirino <juanquirinoc@gmail.com>
2020-05-30 20:39:18 -0600

    Added description for desert organisms

---
+++ src/organisms/desert-organisms.js
@@ -0,0 +1,65 @@
+export default {
+    camel: {
+        name: 'Camel',
+        image: 'camel.jpg',
+        scientificName: 'Camelus dromedarius',
+        description:
+            'A camel is an even-toed ungulate in the genus Camelus that bears distinctive fatty deposits known as "humps" on its back. Camels have long been domesticated and, as livestock, they provide food (milk and meat) and textiles (fiber and felt from hair).'
+    },
+    'fennec-fox': {
+        name: 'Fennec fox',
+        image: 'fennec.jpg',
+        scientificName: 'Vulpes zerda',
+        description:
+            'The fennec fox is a small fox native to the Sahara Desert, the Sinai Peninsula, Arava desert, and the Arabian desert.Its most distinctive feature is its unusually large ears, which also serve to dissipate heat.  Families of fennecs dig out dens in the sand for habitation and protection, which can be as large as 120 m2 and adjoin the dens of other families.'
+    },
+    scorpion: {
+        name: 'Scorpion',
+        image: 'scorpion.jpeg',
+        scientificName: 'Scorpiones',
+        description:
+            'Scorpions are arachnids and have eight legs like their cousins—spiders, mites, and ticks. They can quickly grab an insect with their pincers and whip their telson, the poisonous tip of their tail forward and sting their prey. They use their poison to kill prey and to defend against predators.'
+    },
+    antelope: {
+        name: 'Antelope',
+        image: 'antelope.jpg',
+        scientificName: 'Antilocapra americana',
+        description:
+            'Antelopes comprise a wastebasket taxon (miscellaneous group) within the family Bovidae, encompassing all Old World ruminants that are not bovines, sheep, or goats. Antelopes are considered the sister group to deer within the infraorder Pecora. A group of antelope is called a herd.'
+    },
+    opuntia: {
+        name: 'Opuntia',
+        image: 'opuntia.jpeg',
+        scientificName: 'Opuntia ficus-indica',
+        description:
+            'Opuntia, commonly called prickly pear, is a genus in the cactus family, Cactaceae. Prickly pears are also known as tuna (fruit), sabra, nopal (paddle, plural nopales) from the Nahuatl word nōpalli for the pads, or nostle, from the Nahuatl word nōchtli for the fruit; or paddle cactus.'
+    },
+    cactus: {
+        name: 'Cactus',
+        image: 'cactus.jpg',
+        scientificName: 'Cactáceas',
+        description:
+            'A cactus is a kind of a plant adapted to hot, dry climates. Plants which live this kind of life-style are called xerophytes. Most are succulents, which store water. There are about 127 genera, with over 1750 known species. Most cacti have sharp thorns (stickers) and thick skin. Many cactus flowers are big and beautiful. Some bloom at night and are pollinated by moths and bats. Some cactus fruits are brightly coloured and good to eat: many animals eat cactus fruits.'
+    },
+    eagle: {
+        name: 'Tawny Eagle',
+        image: 'eagle.jpeg',
+        scientificName: 'Aquila rapax',
+        description:
+            'The tawny eagle is a large, long-lived bird of prey. It breeds in most of Africa, both north and south of the Sahara Desert, and across tropical southwestern Asia to India. It is a resident breeder which lays one to three eggs in a stick nest in a tree, crag, or on the ground. Throughout its range, it favours open dry habitats such as semideserts, deserts steppes, or savannah plains.'
+    },
+    viper: {
+        name: 'Sahara horned viper',
+        image: 'viper.jpg',
+        scientificName: 'Cerastes cerastes',
+        description:
+            'The sahara horned viper is a venomous species of viper native to the deserts of northern Africa and parts of the Middle East. It often is easily recognized by the presence of a pair of supraocular "horns", although hornless individuals do occur. The average total length (body and tail) is 30–60 cm, with a maximum total length of 85 cm. Females are larger than males.'
+    },
+    verbena: {
+        name: 'Verbena plant',
+        image: 'verbena.jpg',
+        scientificName: 'Abronia villosa',
+        description:
+            'Desert Sand Verbena are sticky, hairy creepers that have flower stalks up to 10 inches long, with stems trailing up to 3 feet. Leaves are oval with wavy edges 1/2 to 1-1/2 inches long. Sand Verbena can carpet desert washes for miles after abundant winter rains'
+    },
+}
